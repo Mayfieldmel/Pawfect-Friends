eca9ed43bd794be24757003a86973b339a498348
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-08 12:20:14 -0400

    added router export to route

--- controllers/image-routes.js
+++ controllers/image-routes.js
@@ -30,4 +30,4 @@ router.post('/', (req, res) => {
 })
     
 
-    
+  module.exports = router; 
--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
-const imageRoutes = require('./image-routes.js')
+const imageRoutes = require('./image-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
