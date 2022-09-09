912d72ab564b01fae11ad1d6da76e03e646e4767
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-09 16:11:38 -0400

    open & close modal

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,51 +1,20 @@
+var modalEl = document.querySelector("#modal");
+
+
 function modal(event) {
     console.log('click')
+    openModal()
+    document.querySelector("#close").addEventListener("click", closeModal)
+    document.querySelector(".delete").addEventListener("click", closeModal)
 }
 
-// () => {
-//     // Functions to open and close a modal
-//     function openModal($el) {
-//       $el.classList.add('is-active');
-//     }
-  
-//     function closeModal($el) {
-//       $el.classList.remove('is-active');
-//     }
-  
-//     function closeAllModals() {
-//       (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-//         closeModal($modal);
-//       });
-//     }
-  
-//     // Add a click event on buttons to open a specific modal
-//     (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-//       const modal = $trigger.dataset.target;
-//       const $target = document.getElementById(modal);
-  
-//       $trigger.addEventListener('click', () => {
-//         openModal($target);
-//       });
-//     });
-  
-//     // Add a click event on various child elements to close the parent modal
-//     (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-//       const $target = $close.closest('.modal');
-  
-//       $close.addEventListener('click', () => {
-//         closeModal($target);
-//       });
-//     });
-  
-//     // Add a keyboard event to close all modals
-//     document.addEventListener('keydown', (event) => {
-//       const e = event || window.event;
-  
-//       if (e.keyCode === 27) { // Escape key
-//         closeAllModals();
-//       }
-//     });
-// }
+    // Functions to open and close a modal
+    function openModal() {
+      modalEl.classList.add('is-active');
+    }
+    function closeModal($el) {
+        modalEl.classList.remove('is-active');
+    }
   
 
   document.querySelector("#add-post-btn").addEventListener("click", modal);
