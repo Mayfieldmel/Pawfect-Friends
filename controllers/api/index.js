48c206f3945a29341458dcbd9efd9d3024277d4b
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-11 17:56:21 -0400

    put style.css in proper folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,6 +6,6 @@ const postRoutes = require("./post-routes");
 
 router.use("/pets", petRoutes);
 router.use("/posts", postRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
