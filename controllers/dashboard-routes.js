bbe7ec099e1465e103c1d0e6152876d8415fae27
rramosx11 <rodolforamosd11@gmail.com>
2022-09-11 16:05:26 -0400

    post load on dashboard

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const { Post, Pet, Comment } = require("../models");
+// const withAuth = require("../utils/auth");
+
+// get all posts for dashboards
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "post_text", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "pet_id", "created_at"],
+        include: {
+          model: Pet,
+          attributes: ["pet_name"],
+        },
+      },
+      {
+        model: Pet,
+        attributes: ["pet_name"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      console.log(posts);
+
+      res.render("dashboard", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
