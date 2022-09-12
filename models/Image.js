09d57dc794def3cfcaf765ae87a5640027390480
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-12 12:02:59 -0400

    add profile to Image model

--- models/Image.js
+++ models/Image.js
@@ -1,32 +1,6 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
-// const Image = sequelize.define("image", {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     image: {
-//         type: DataTypes.BLOB("long"),
-//       },
-//     type: {
-//       type: DataTypes.STRING,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//     },
-//     size: {
-//       type: DataTypes.INTEGER,
-//     },
-//     post_id: {
-//       type: DataTypes.INTEGER,
-//     }
-    
-//   });
-
-
 class Image extends Model {}
 
 Image.init( 
