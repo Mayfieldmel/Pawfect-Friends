e20e5decab03c923cd80edc47bcd6d81242de8c8
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-09-13 19:56:53 -0400

    images displaying with posts

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, Pet, Comment, Image } = require("../models");
 const { withAuth, withAuthSign } = require("../utils/auth");
+const sortArray = require('sort-array');
 
 // GET profile/update
 router.get("/update", withAuthSign, (req, res) => {
@@ -17,27 +18,6 @@ router.get("/add-post", withAuthSign, (req, res) => {
   });
 });
 
-//  GET /profile
-router.get("/", withAuthSign, (req, res) => {
-    console.log("session data", req.session);
-        console.log('======================');
-        // get all posts for dashboard
-        Pet.findAll({
-          where: {
-            id: req.session.pet_id
-          },
-        })
-          .then(dbPetData => {
-              // serialize data
-            const pets = dbPetData.map(pet => pet.get({ plain: true }));
-            console.log("pets:", pets)
-            res.render('profile', { pets: pets[0], loggedIn: true });
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-          });
-      });
  
       // PUT /profile/img
 router.put("/img", (req, res) => {
@@ -106,80 +86,108 @@ router.put("/img", (req, res) => {
 //   });
 
 // SUCCESS GETTING ONE ARRAY W/ POSTS & IMAGES
-//   GET /profile/display
-router.get('/display', withAuthSign, (req, res) => {
+//   GET /profile/
+router.get('/', withAuthSign, async (req, res) => {
     console.log("session data", req.session);
     console.log('======================');
     // get all posts & images for dashboard
-    Post.findAll({
-        where: {
-            pet_id: req.session.pet_id
-        },
-        attributes: [
-            'id',
-            'post_text',
-            'created_at',
-        ],
-        include: [
-            {
-                model: Pet,
-                attributes: ['pet_name', 'email', 'password']
+
+    try {
+
+        const petData = await Pet.findAll({
+            where: {
+              id: req.session.pet_id
             },
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'pet_id', 'created_at'],
-                include: {
-                model: Pet,
-                attributes: ['pet_name']
+            raw: true
+          })
+
+        const postData = await Post.findAll({
+            where: {
+                pet_id: req.session.pet_id
+            },
+            attributes: [
+                'id',
+                'post_text',
+                'created_at',
+            ],
+            include: [
+                {
+                    model: Pet,
+                    attributes: ['pet_name', 'email', 'password']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'pet_id', 'created_at'],
+                    include: {
+                    model: Pet,
+                    attributes: ['pet_name']
+                    }
                 }
-            }
-        ],
-        order: [
-            ['created_at', 'DESC']
-        ]
-    }).then(dbPostData => {
-    Image.findAll({
-        where: {
-            pet_id: req.session.pet_id
-        },
-        attributes: ['image', 'created_at'],
-        include: [
-            {
-                model: Pet,
-                attributes: ['pet_name']
-            }
-            // ,
-            // {
-            //     model: Comment,
-            //     attributes: ['id', 'comment_text', 'post_id', 'pet_id', 'created_at'],
-            //     include: {
-            //     model: Pet,
-            //     attributes: ['pet_name']
-            //     }
-            // }
-        ],
-        order: [
-            ['created_at', 'DESC']
-        ]
-    })
-    .then(dbImageData => {
-        const dbAllData = [...dbPostData, ...dbImageData]
-        console.log(dbAllData)
-        return dbAllData;
-    })
-    .then(dbPostData => {
-        // serialize data
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      console.log(posts)
-      res.render('profile', { posts, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-})
-      
-  });
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            raw: true
+        });
+
+        const imgData = await Image.findAll({
+            where: {
+                pet_id: req.session.pet_id
+            },
+            attributes: ['image', 'created_at'],
+            include: [
+                {
+                    model: Pet,
+                    attributes: ['pet_name']
+                }
+                // ,
+                // {
+                //     model: Comment,
+                //     attributes: ['id', 'comment_text', 'post_id', 'pet_id', 'created_at'],
+                //     include: {
+                //     model: Pet,
+                //     attributes: ['pet_name']
+                //     }
+                // }
+            ],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            raw: true
+        });
+
+        console.log(postData);
+        console.log(imgData);
+
+        const combinedArr = [...postData, ...imgData];
+
+        console.log(combinedArr);
+
+        const dataArr = sortArray(combinedArr, {
+            by: 'created_at',
+            order: 'desc'
+          });
+    
+        // return res.render('profile', { posts, loggedIn: true });
+
+
+          console.log(dataArr);
+
+        res.render('profile', { 
+            pets: petData[0], 
+            dataArr: dataArr, 
+            loggedIn: true 
+        });
+
+
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500).json(err);
+    };
+
+});
+
+
 
 // FAILED PROMISE.ALL ATTEMPT
 // router.get('/', withAuthSign, (req, res) => {
