2aca6119ae20fe2b99cc3a1fc6e138187c9399c5
rramosx11 <rodolforamosd11@gmail.com>
2022-09-12 20:39:32 -0400

    added a edit save button

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -51,4 +51,39 @@ router.get("/", withAuthSign, (req, res) => {
     });
 });
 
+router.get("/edit/:id", (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "post_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "pet_id", "created_at"],
+        include: {
+          model: Pet,
+          attributes: ["pet_name"],
+        },
+      },
+      {
+        model: Pet,
+        attributes: ["pet_name"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
