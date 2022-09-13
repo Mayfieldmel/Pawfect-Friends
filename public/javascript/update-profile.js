2f968f2c55c06f5ddd7d0b1551b028c006dba35c
Brian King <bnk5532@gmail.com>
2022-09-13 00:33:35 -0400

    icon added to menu w/hover, password validation added -- still developing

--- public/javascript/update-profile.js
+++ public/javascript/update-profile.js
@@ -5,21 +5,54 @@ async function updateFormHandler(event) {
     const password = document.querySelector('#password-update').value.trim();
     
     
-    if (email && password) {
-        const response = await fetch(`/api/pets/${req.session.pet_id}`, {
-            method: 'put',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
+//     if (email && password) {
+//         const response = await fetch(`/api/pets/${req.session.pet_id}`, {
+//             method: 'put',
+//         body: JSON.stringify({
+//           email,
+//           password
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
   
-      if (response.ok) {
-        console.log(response)
-        // document.location.replace('/profile');
-      } 
-    }
-  }
+//       if (response.ok) {
+//         console.log(response)
+//         // document.location.replace('/profile');
+//       } 
+//     }
+//   }
 
- document.querySelector('#update-pro').addEventListener('submit', updateFormHandler);
+  function checkForm()
+  {
+   var oldPass=document.getElementById("oldP").value;
+   var newPass=document.getElementById("newP").value;
+   var confirmPass =document.getElementById("confirmP").value;
+
+   if(oldPass!=""&&newPass!=""&&confirmPass!="")
+   {
+     if(oldPass!=newPass)
+     {
+       if(newPass==confirmPass)
+        {
+         return true;
+        }
+        else
+         {
+           alert("Confirm password is not same as you new password.");
+           return false;
+         }
+     }
+     else
+    {
+     alert("This Is Your Old Password,Please Provide A New Password");
+     return false;
+    }
+   }
+   else
+   {
+    alert("All Fields Are Required");
+    return false;
+   }
+}}
+
+//  document.querySelector('#update-pro').addEventListener('submit', updateFormHandler);
